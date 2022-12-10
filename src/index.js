b28e3192b910a2c08c31bf7c68942e0f3efe1af0
jungHyeonS <fbtls25@naver.com>
2022-12-10 20:51:51 +0900

    feat : store and reducer

--- src/index.js
+++ src/index.js
@@ -1,26 +1,20 @@
+import { createStore } from "redux";
+
+
+//state : application 변경되는 데이터
+//store : 데이터 저장소
+//reducer : data를 modify해주는 함수로 reducer가 return하는것을 application에 있는 데이터가 된다
+
 const add = document.getElementById("add")
 const minus = document.getElementById("minus")
 const number = document.querySelector("span");
 
 
-let count = 0;
-
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerHTML = count;
+const countModifer = (count = 0) => {
+  //리듀서만 데이터를 수정할수있다
+  return count;
 }
 
-const handleAdd = () => {
-  count++;
-  updateText()
-}
+const countStore = createStore(countModifer);
 
-const handleMinus = () => {
-  count--;
-  updateText()
-}
-
-add.addEventListener("click",handleAdd)
-minus.addEventListener("click",handleMinus)
+console.log(countStore.getState())
