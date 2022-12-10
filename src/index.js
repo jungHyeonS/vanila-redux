16321c69f3d04207462f25bb1e44885e393a2e78
jungHyeonS <fbtls25@naver.com>
2022-12-10 21:05:46 +0900

    feat : Redux Subscribe

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import { createStore } from "redux";
 //reducer : data를 modify해주는 함수로 reducer가 return하는것을 application에 있는 데이터가 된다
 //action : redux에서 function을 부를때 쓰는 두번째 파라미터 혹은 argument로 reducer와 소통하기 위한 방법
 //Reducer에게 Action을 보내는 방법 : store.dispatch({key:value})
+//subscribe : store안에있는 데이터 변화 감지
 
 const add = document.getElementById("add")
 const minus = document.getElementById("minus")
 const number = document.querySelector("span");
 
 
+
+//리듀서
 const countModifer = (count = 0,action) => {
   //리듀서만 데이터를 수정할수있다
 
@@ -25,9 +28,13 @@ const countModifer = (count = 0,action) => {
 
 const countStore = createStore(countModifer);
 
-countStore.dispatch({type:"ADD"})
-countStore.dispatch({type:"ADD"})
-countStore.dispatch({type:"ADD"})
-countStore.dispatch({type:"ADD"})
-countStore.dispatch({type:"MINUS"})
-console.log(countStore.getState())
+
+const onChange = () => {
+  number.innerText = countStore.getState()
+}
+
+//스토어 데이터 변화 감지
+countStore.subscribe(onChange)
+
+add.addEventListener("click",() => countStore.dispatch({type:"ADD"}))
+minus.addEventListener("click",()=> countStore.dispatch({type:"MINUS"}))
