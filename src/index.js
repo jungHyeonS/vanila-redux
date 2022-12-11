fd27b8ac75908f95daf6ca543aed1b0ebf0c7c3d
jungHyeonS <fbtls25@naver.com>
2022-12-11 11:33:09 +0900

    feat : Redux State Mutation

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ const input = document.querySelector("input");
 const ul = document.querySelector("ul")
 
 
+//action type 변수화
 const ADD_TODO = "ADD_TODO"
 const DELETE_TODO = "DELETE_TODO"
 
+
+//리듀서 생성
 const reducer = (state = [],action) => {
   switch(action.type){
     case ADD_TODO:
+      //절때로 state를 mutate해서는 안되기에 새로운 state를 생성하고 리턴해준다
       return [...state,{text:action.text,id:action.id}]
     case DELETE_TODO:
       return []
@@ -17,9 +21,12 @@ const reducer = (state = [],action) => {
       return state 
   }
 }
+
+//스토어 생성
 const store = createStore(reducer);
 
 
+//스토어 값 변화 감지
 store.subscribe(()=>{
   console.log(store.getState())
 })
@@ -35,6 +42,7 @@ const onSubmit = e => {
   const toDo = input.value;
   input.value = "";
   // createTodo(toDo)
+  //디스패치를 통해서 액션에 타입과 페이로드 값들을 보낸다
   store.dispatch({type:ADD_TODO,text:toDo,id:Date.now()})
 }
 
